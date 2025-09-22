280f93bf3f13747f9aaf2ed405115809727337b8
Alcino Cunha <alcino@di.uminho.pt>
2025-09-22 06:41:21 +0200

    results gpt 1 pos 1 neg

--- gpt.py
+++ gpt.py
@@ -11,7 +11,7 @@ with open(sys.argv[1], 'r') as f:
 
 client = OpenAI()
 
-instances = 3
+instances = 1
 llm = "gpt-5-2025-08-07"
 
 with open(sys.argv[2], 'r') as f, open(llm+'_'+sys.argv[2], 'w') as g:
