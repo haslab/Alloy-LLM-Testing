a28e50a819a0fedbd75b79b20c9cdc8451ae34b8
Alcino Cunha <alcino@di.uminho.pt>
2025-09-23 06:54:31 +0200

    Results gpt multi4

--- gpt.py
+++ gpt.py
@@ -11,7 +11,7 @@ with open(sys.argv[1], 'r') as f:
 
 client = OpenAI()
 
-instances = 2
+instances = 4
 llm = "gpt-5-2025-08-07"
 
 with open(sys.argv[2], 'r') as f, open(llm+'_'+sys.argv[2], 'w') as g:
